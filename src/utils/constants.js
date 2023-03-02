3b9ccc2788fca46c5ecc8a44f95a7e181dfc5bbc
Agustin Navarro <xAgustin93@gmail.com>
2023-03-02 20:42:55 +0100

    Add server url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 export const ENV = {
-  SERVER_HOST: "http://localhost:1337",
-  API_URL: "http://localhost:1337/api",
+  SERVER_HOST: "https://ecommerce-strapi.up.railway.app",
+  API_URL: "https://ecommerce-strapi.up.railway.app/api",
   ENDPOINTS: {
     AUTH: {
       REGISTER: "auth/local/register",
